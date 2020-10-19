30319b017fe1c99d4336c8787012acf29daab602
Pradeep Jerome <prjerome@gmail.com>
2020-10-19 12:55:54 +0200

    Merged src/components/SearchBar/Searchbar.js diff from master, into codecademy_instructions branch src/components/SearchBar/Searchbar.js.

--- src/components/SearchBar/Searchbar.js
+++ src/components/SearchBar/Searchbar.js
@@ -1,15 +1,6 @@
 import React from 'react';
 import './SearchBar.css';
 
-class Searchbar extends React.Component {
-    render(
-        return(
-            <input name="whereInput" />
-            <input name="searchBusinessInput" />
-        )
-    )
-}
-
 //API docs https://www.yelp.com/developers/documentation/v3/business_search
 
 const searchBusinessInput = //optional. get value from user input into 'search Businesses' searchbar eg document.querySelector 'input[name="searchBusinessInput"']
@@ -18,13 +9,27 @@ const whereInput = //required. get value from user input into 'where' searchbar 
 
 const sortByClick = //optional. get value from user click on link 'best match', 'highest rated' or 'most reviewed'. Get value using event handler onclick. Then e.target.value.
 
-const queryString = `searchBusinessInput=${term}` + `location=${whereInput}` + `sort_By=${sortByClick}`;
+const sortByOptions = {
+    "Best Match": "best_match",
+    "Highest Rated": "rating",
+    "Most Reviewed": "review_count",
+   }
+
+/*const queryString = `searchBusinessInput=${term}` + `location=${whereInput}` + `sort_By={sortByIOptions[0, 1 || 2]}`; */
 
 const url = 'https://api.yelp.com/v3/businesses/search' + queryString;
 
-
-const sortByOptions = {
-    method: 'GET',  
-  }
-
 const response = fetch (url, sortByOptions);
+
+class Searchbar extends React.Component {
+    renderSortByOptions() {
+        return(
+            <div>
+                <li id="Best Match">{sortByOptions["Best Match"]}</li>        
+                <li id="Highest Rated">{sortByOptions["Highest Rated"] }</li>
+                <li id="Most Reviewed">{sortByOptions["Most Reviewed"] }</li>
+            </div>
+            
+        
+    }
+}
