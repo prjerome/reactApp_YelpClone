ab5d7452ce76d791372c016b7c335f46768d6e35
Pradeep Jerome <prjerome@gmail.com>
2020-10-22 14:12:43 +0200

    Updates

---
+++ precache-manifest.45b4564ec1b6c523345cc4e2ca32cecd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2207779422ea2cd148d951e3fee70808",
+    "url": "/reactApp_YelpClone/index.html"
+  },
+  {
+    "revision": "22e3e5f9e277f9c6e187",
+    "url": "/reactApp_YelpClone/static/css/main.b287f705.chunk.css"
+  },
+  {
+    "revision": "b0b1b430d5e674015614",
+    "url": "/reactApp_YelpClone/static/js/2.35ce6af7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/reactApp_YelpClone/static/js/2.35ce6af7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22e3e5f9e277f9c6e187",
+    "url": "/reactApp_YelpClone/static/js/main.1b1a5c05.chunk.js"
+  },
+  {
+    "revision": "f0724e1f9c6abe0845b3",
+    "url": "/reactApp_YelpClone/static/js/runtime-main.7ae84d5d.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/reactApp_YelpClone/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "082e88ec250194e6416adc8772adea4e",
+    "url": "/reactApp_YelpClone/static/media/background_search_mobile.082e88ec.webp"
+  }
+]);
