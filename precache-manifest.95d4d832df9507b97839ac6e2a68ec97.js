a5659db26fbad5dbf4018abbb5642a532acded7c
Pradeep Jerome <prjerome@gmail.com>
2020-11-18 14:50:39 +0100

    Updates

---
+++ precache-manifest.95d4d832df9507b97839ac6e2a68ec97.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0e09e9b43a4881437d81b3cca6d2638",
+    "url": "//prjerome.github.io/reactApp_YelpClone/index.html"
+  },
+  {
+    "revision": "ed886003ab13539be32a",
+    "url": "//prjerome.github.io/reactApp_YelpClone/static/css/main.e61ace4f.chunk.css"
+  },
+  {
+    "revision": "b0b1b430d5e674015614",
+    "url": "//prjerome.github.io/reactApp_YelpClone/static/js/2.35ce6af7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "//prjerome.github.io/reactApp_YelpClone/static/js/2.35ce6af7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed886003ab13539be32a",
+    "url": "//prjerome.github.io/reactApp_YelpClone/static/js/main.f73484d8.chunk.js"
+  },
+  {
+    "revision": "51e45a7c8589223de22b",
+    "url": "//prjerome.github.io/reactApp_YelpClone/static/js/runtime-main.0e287552.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "//prjerome.github.io/reactApp_YelpClone/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "082e88ec250194e6416adc8772adea4e",
+    "url": "//prjerome.github.io/reactApp_YelpClone/static/media/background_search_mobile.082e88ec.webp"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-app/precache-manifest.c9495b362cb0349f42c97b76bc17a623.js"
+  "//prjerome.github.io/reactApp_YelpClone/precache-manifest.95d4d832df9507b97839ac6e2a68ec97.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("//prjerome.github.io/reactApp_YelpClone/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
